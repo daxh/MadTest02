683d69357379ef7b36fea645d625ad94cbd96d47
Denis Suprun <suprundenis@gmail.com>
2017-01-11 12:57:48 +0300

    + Adding basic ClassicRecyclerViewAdapter with necessary stuff + Adding demo data + Fixing minor layout bug in item_view + Combining everything in a ClassicRecyclerViewUsageExampleActivity

---
+++ app/src/main/java/com/daxh/explore/madtest02/classic/ClassicRecyclerViewAdapter.java
@@ -0,0 +1,38 @@
+package com.daxh.explore.madtest02.classic;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.daxh.explore.madtest02.R;
+import com.daxh.explore.madtest02.common.Item;
+import com.daxh.explore.madtest02.common.ItemViewHolder;
+
+import java.util.ArrayList;
+
+public class ClassicRecyclerViewAdapter extends RecyclerView.Adapter<ItemViewHolder> {
+
+    private ArrayList<Item> items;
+
+    public ClassicRecyclerViewAdapter(ArrayList<Item> items) {
+        this.items = items;
+    }
+
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_view, parent, false);
+        return new ItemViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
+        holder.bindItem(items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items == null ? 0 : items.size();
+    }
+}
--- app/src/main/java/com/daxh/explore/madtest02/classic/ClassicRecyclerViewUsageExampleActivity.java
+++ app/src/main/java/com/daxh/explore/madtest02/classic/ClassicRecyclerViewUsageExampleActivity.java
@@ -2,14 +2,40 @@ package com.daxh.explore.madtest02.classic;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.daxh.explore.madtest02.R;
+import com.daxh.explore.madtest02.common.Item;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class ClassicRecyclerViewUsageExampleActivity extends AppCompatActivity {
+
+    private RecyclerView rvItems;
+    private LinearLayoutManager llmItems;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classic_recyclerview_usage_example);
+
+        llmItems = new LinearLayoutManager(this);
+        rvItems = (RecyclerView) findViewById(R.id.rvItems);
+        rvItems.setLayoutManager(llmItems);
+
+        // Preparing data
+        String [] dataArray = getResources().getStringArray(R.array.lorem_ipsum);
+        ArrayList<String> strings = new ArrayList<>(Arrays.asList(dataArray[0].split(" ")));
+        ArrayList<Item> items = new ArrayList<>();
+        for (String s : strings) items.add(new Item(s));
+
+        // Creating adapter
+        ClassicRecyclerViewAdapter adapter = new ClassicRecyclerViewAdapter(items);
+
+        // Setting up adapter for RecyclerView
+        rvItems.setAdapter(adapter);
     }
 }
