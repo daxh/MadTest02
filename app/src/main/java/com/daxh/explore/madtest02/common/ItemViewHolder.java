e787ee8e1aaa34fb715a1fdb981de87028ea80fa
Denis Suprun <suprundenis@gmail.com>
2017-01-11 12:29:25 +0300

    + Extending ItemViewHolder implementation

--- app/src/main/java/com/daxh/explore/madtest02/common/ItemViewHolder.java
+++ app/src/main/java/com/daxh/explore/madtest02/common/ItemViewHolder.java
@@ -2,9 +2,23 @@ package com.daxh.explore.madtest02.common;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
+import com.daxh.explore.madtest02.R;
 
 public class ItemViewHolder extends RecyclerView.ViewHolder {
+    private TextView tvText;
+    private Item item;
+
     public ItemViewHolder(View itemView) {
         super(itemView);
+
+        tvText = (TextView) itemView.findViewById(R.id.tvText);
+    }
+
+    public void bindItem(Item item) {
+        this.item = item;
+
+        tvText.setText(this.item.getText());
     }
 }
