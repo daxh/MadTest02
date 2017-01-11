23d4b54be9e2c5b046b4b337bfe467e70e5aacc5
Denis Suprun <suprundenis@gmail.com>
2017-01-11 12:07:10 +0300

    + Adding ItemViewHolder that probably will be used for all examples

---
+++ app/src/main/java/com/daxh/explore/madtest02/common/ItemViewHolder.java
@@ -0,0 +1,10 @@
+package com.daxh.explore.madtest02.common;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class ItemViewHolder extends RecyclerView.ViewHolder {
+    public ItemViewHolder(View itemView) {
+        super(itemView);
+    }
+}
