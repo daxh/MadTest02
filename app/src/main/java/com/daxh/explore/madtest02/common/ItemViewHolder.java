77c535d58ce2e1c1dd0ee95b4c066425f6e68f6e
Denis Suprun <suprundenis@gmail.com>
2017-01-11 15:02:36 +0300

    + Handling recyclerView items clicks

--- app/src/main/java/com/daxh/explore/madtest02/common/ItemViewHolder.java
+++ app/src/main/java/com/daxh/explore/madtest02/common/ItemViewHolder.java
@@ -3,16 +3,19 @@ package com.daxh.explore.madtest02.common;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.daxh.explore.madtest02.R;
 
-public class ItemViewHolder extends RecyclerView.ViewHolder {
+public class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
     private TextView tvText;
     private Item item;
 
     public ItemViewHolder(View itemView) {
         super(itemView);
 
+        itemView.setOnClickListener(this);
         tvText = (TextView) itemView.findViewById(R.id.tvText);
     }
 
@@ -20,5 +23,10 @@ public class ItemViewHolder extends RecyclerView.ViewHolder {
         this.item = item;
 
         tvText.setText(this.item.getText());
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(view.getContext(), item.getText() + " clicked!", Toast.LENGTH_SHORT).show();
     }
 }
