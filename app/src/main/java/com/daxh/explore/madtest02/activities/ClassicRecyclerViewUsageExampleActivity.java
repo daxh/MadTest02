399530bf5b3ace331d3753c052109f4c68a93abf
Denis Suprun <suprundenis@gmail.com>
2017-01-11 11:33:14 +0300

    + Empty ClassicRecyclerViewUsageExampleActivity added to project with related stuff

---
+++ app/src/main/java/com/daxh/explore/madtest02/activities/ClassicRecyclerViewUsageExampleActivity.java
@@ -0,0 +1,15 @@
+package com.daxh.explore.madtest02.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.daxh.explore.madtest02.R;
+
+
+public class ClassicRecyclerViewUsageExampleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classic_recyclerview_usage_example);
+    }
+}
--- app/src/main/java/com/daxh/explore/madtest02/activities/MainActivity.java
+++ app/src/main/java/com/daxh/explore/madtest02/activities/MainActivity.java
@@ -1,9 +1,11 @@
 package com.daxh.explore.madtest02.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.daxh.explore.madtest02.R;
+import com.daxh.explore.madtest02.utils.BindingUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BindingUtils.bindButton(this, R.id.btClassicRecycleView, () -> new Intent(this, ClassicRecyclerViewUsageExampleActivity.class));
     }
 }
