ab71742bb91bc022437d1f7ac386959f557ef5b2
Denis Suprun <suprundenis@gmail.com>
2017-01-12 07:24:57 +0300

    + ClassicExamplesActivity added

--- app/src/main/java/com/daxh/explore/madtest02/activities/MainActivity.java
+++ app/src/main/java/com/daxh/explore/madtest02/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.daxh.explore.madtest02.R;
+import com.daxh.explore.madtest02.classic.ClassicExamplesActivity;
 import com.daxh.explore.madtest02.simple.SimpleRecyclerViewActivity;
 import com.daxh.explore.madtest02.utils.BindingUtils;
 
@@ -16,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BindingUtils.bindButton(this, R.id.btSimpleRecycleView, () -> new Intent(this, SimpleRecyclerViewActivity.class));
+        BindingUtils.bindButton(this, R.id.btClassicRecycleView, () -> new Intent(this, ClassicExamplesActivity.class));
     }
 }
---
+++ app/src/main/java/com/daxh/explore/madtest02/classic/ClassicExamplesActivity.java
@@ -0,0 +1,15 @@
+package com.daxh.explore.madtest02.classic;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.daxh.explore.madtest02.R;
+
+public class ClassicExamplesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classic_examples);
+    }
+}
