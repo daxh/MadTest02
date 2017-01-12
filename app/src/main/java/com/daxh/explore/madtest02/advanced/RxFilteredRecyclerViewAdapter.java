c74a64d0bf55a71c5ef11c5de688478bf6c0aa4a
Denis Suprun <suprundenis@gmail.com>
2017-01-12 20:46:31 +0300

    + Adding Optionals to RxFilteredRecyclerViewAdapter

--- app/src/main/java/com/daxh/explore/madtest02/advanced/RxFilteredRecyclerViewAdapter.java
+++ app/src/main/java/com/daxh/explore/madtest02/advanced/RxFilteredRecyclerViewAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.annimon.stream.Optional;
 import com.daxh.explore.madtest02.R;
 import com.daxh.explore.madtest02.common.Item;
 import com.daxh.explore.madtest02.common.ItemViewHolder;
@@ -19,29 +20,30 @@ import rx.subscriptions.CompositeSubscription;
 
 public class RxFilteredRecyclerViewAdapter extends RecyclerView.Adapter<ItemViewHolder> {
 
-    // TODO: Add Optionals
-    private ArrayList<Item> originalItems;
-    private ArrayList<Item> filteredItems;
+    private Optional<ArrayList<Item>> originalItems;
+    private Optional<ArrayList<Item>> filteredItems;
 
     private CompositeSubscription compositeSubscription = new CompositeSubscription();
 
     public RxFilteredRecyclerViewAdapter(ArrayList<Item> items, Observable<String> keywords) {
-        this.originalItems = items;
-        filteredItems = new ArrayList<>(this.originalItems);
+        this.originalItems = Optional.ofNullable(items);
+        this.originalItems
+                .executeIfPresent(oi -> filteredItems = Optional.of(new ArrayList<>(oi)))
+                .executeIfAbsent(() -> filteredItems = Optional.of(new ArrayList<Item>()));
 
-        keywords
-                .debounce(10, TimeUnit.MILLISECONDS)
-                .subscribe(keyword -> { compositeSubscription.add(Observable.from(originalItems)
+        Optional.ofNullable(keywords).ifPresent(k -> k
+                .debounce(100, TimeUnit.MILLISECONDS)
+                .subscribe(keyword -> { compositeSubscription.add(Observable.from(originalItems.get())
                         .subscribeOn(Schedulers.computation())
                         .filter(item -> item.getText().contains(keyword))
                         .toList()
                         .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(filteredItems -> {
-                            this.filteredItems.clear();
-                            this.filteredItems = (ArrayList<Item>) filteredItems;
+                        .subscribe(fi -> {
+                            filteredItems.ifPresent(ArrayList::clear);
+                            filteredItems = Optional.of(new ArrayList<>(fi));
                             notifyDataSetChanged();
-                        }, Throwable::printStackTrace));
-                }, Throwable::printStackTrace);
+                        }));
+                }));
     }
 
     @Override
@@ -53,12 +55,12 @@ public class RxFilteredRecyclerViewAdapter extends RecyclerView.Adapter<ItemView
 
     @Override
     public void onBindViewHolder(ItemViewHolder holder, int position) {
-        holder.bindItem(filteredItems.get(position));
+        filteredItems.ifPresent(fi -> holder.bindItem(fi.get(position)));
     }
 
     @Override
     public int getItemCount() {
-        return filteredItems == null ? 0 : filteredItems.size();
+        return filteredItems.mapToInt(ArrayList::size).orElse(0);
     }
 
     @Override
