371848e72bb6c473613d4a850811b008c51c24fd
Denis Suprun <suprundenis@gmail.com>
2017-01-13 11:12:34 +0300

    + SortedRecyclerViewActivity now allows to sort items

--- app/src/main/java/com/daxh/explore/madtest02/classic/SortedRecyclerViewActivity.java
+++ app/src/main/java/com/daxh/explore/madtest02/classic/SortedRecyclerViewActivity.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.daxh.explore.madtest02.R;
 import com.daxh.explore.madtest02.common.Item;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
 
 public class SortedRecyclerViewActivity extends AppCompatActivity {
+
+    private Button btSortingOff;
+    private Button btSortByLength;
+    private Button btSortByOrder;
 
     private RecyclerView rvItems;
     private LinearLayoutManager llmItems;
@@ -37,5 +44,51 @@ public class SortedRecyclerViewActivity extends AppCompatActivity {
 
         // Setting up adapter for RecyclerView
         rvItems.setAdapter(adapter);
+
+        btSortingOff = (Button) findViewById(R.id.btSortingOff);
+        btSortByLength = (Button) findViewById(R.id.btSortByLength);
+        btSortByOrder = (Button) findViewById(R.id.btSortByOrder);
+
+        btSortingOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapter.sort(null);
+            }
+        });
+
+        btSortByLength.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapter.sort(new LengthComparator());
+            }
+        });
+        btSortByOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapter.sort(new OrderComparator());
+            }
+        });
+    }
+
+    public static class LengthComparator implements Comparator<Item> {
+        @Override
+        public int compare(Item item1, Item item2) {
+            if (item1.getText().length() < item2.getText().length()) return -1;
+            if (item1.getText().length() > item2.getText().length()) return 1;
+            return 0;
+        }
+    }
+
+    public static class OrderComparator implements Comparator<Item> {
+        @Override
+        public int compare(Item item1, Item item2) {
+            final String str1 = item1.getText();
+            final String str2 = item2.getText();
+            int res = String.CASE_INSENSITIVE_ORDER.compare(str1, str2);
+            if (res == 0) {
+                res = str1.compareTo(str2);
+            }
+            return res;
+        }
     }
 }
--- app/src/main/java/com/daxh/explore/madtest02/classic/SortedRecyclerViewAdapter.java
+++ app/src/main/java/com/daxh/explore/madtest02/classic/SortedRecyclerViewAdapter.java
@@ -4,19 +4,29 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
 
 import com.daxh.explore.madtest02.R;
 import com.daxh.explore.madtest02.common.Item;
 import com.daxh.explore.madtest02.common.ItemViewHolder;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class SortedRecyclerViewAdapter extends RecyclerView.Adapter<ItemViewHolder> {
 
-    private ArrayList<Item> items;
+    private ArrayList<Item> originalItems;
+    private ArrayList<Item> sortedItems;
+
+    private ItemFilter itemFilter;
+    private Comparator<Item> comparator = null;
 
     public SortedRecyclerViewAdapter(ArrayList<Item> items) {
-        this.items = items;
+        this.originalItems = items;
+
+        sortedItems = new ArrayList<>(this.originalItems);
+        itemFilter = new ItemFilter();
     }
 
     @Override
@@ -28,11 +38,48 @@ public class SortedRecyclerViewAdapter extends RecyclerView.Adapter<ItemViewHold
 
     @Override
     public void onBindViewHolder(ItemViewHolder holder, int position) {
-        holder.bindItem(items.get(position));
+        holder.bindItem(sortedItems.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return items == null ? 0 : items.size();
+        return sortedItems == null ? 0 : sortedItems.size();
+    }
+
+    public void sort(Comparator<Item> comparator){
+        this.comparator = comparator;
+        itemFilter.filter("");
+    }
+
+    // According to official documentation:
+    // https://developer.android.com/reference/android/widget/Filter.html
+    // This class allows to execute filtering operations
+    // asynchronously and properly cancel that iterations
+    // that became un-relevant. Obviously, we using it here,
+    // but for sorting. Probably there is a more true way
+    // to do it, but right now I didn't know it.
+    public class ItemFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence keyword) {
+            FilterResults results = new FilterResults();
+            final ArrayList<Item> tempFilterList = new ArrayList<>(originalItems);
+
+            if (comparator != null) {
+                Collections.sort(tempFilterList, comparator);
+            }
+
+            results.values = tempFilterList;
+            results.count = tempFilterList.size();
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            sortedItems.clear();
+            sortedItems = (ArrayList<Item>) filterResults.values;
+            notifyDataSetChanged();
+        }
     }
 }
