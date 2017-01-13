7b33ab2159ddf63a56e998989805c542a78952b6
Denis Suprun <suprundenis@gmail.com>
2017-01-13 22:05:40 +0300

    + Displaying items with headers implementation added

--- app/src/main/java/com/daxh/explore/madtest02/simple/RecyclerViewWithHeadersActivity.java
+++ app/src/main/java/com/daxh/explore/madtest02/simple/RecyclerViewWithHeadersActivity.java
@@ -6,10 +6,12 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.daxh.explore.madtest02.R;
+import com.daxh.explore.madtest02.common.Header;
 import com.daxh.explore.madtest02.common.Item;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 
 
 public class RecyclerViewWithHeadersActivity extends AppCompatActivity {
@@ -29,11 +31,23 @@ public class RecyclerViewWithHeadersActivity extends AppCompatActivity {
         // Preparing data
         String [] dataArray = getResources().getStringArray(R.array.lorem_ipsum);
         ArrayList<String> strings = new ArrayList<>(Arrays.asList(dataArray[0].split(" ")));
-        ArrayList<Item> items = new ArrayList<>();
-        for (String s : strings) items.add(new Item(s));
+        LinkedHashMap<Header, ArrayList<Item>> dataset = new LinkedHashMap<>();
+        final int step = 5;
+        for (int i = 0; i < strings.size();) {
+            Header header = new Header(strings.get(i));
+            i++;
+
+            ArrayList<Item> items = new ArrayList<>();
+            for (int j = 0; j < step && i < strings.size(); j++) {
+                items.add(new Item(strings.get(i)));
+                i++;
+            }
+
+            dataset.put(header, items);
+        }
 
         // Creating adapter
-        RecyclerViewWithHeadersAdapter adapter = new RecyclerViewWithHeadersAdapter(items);
+        RecyclerViewWithHeadersAdapter adapter = new RecyclerViewWithHeadersAdapter(dataset);
 
         // Setting up adapter for RecyclerView
         rvItems.setAdapter(adapter);
--- app/src/main/java/com/daxh/explore/madtest02/simple/RecyclerViewWithHeadersAdapter.java
+++ app/src/main/java/com/daxh/explore/madtest02/simple/RecyclerViewWithHeadersAdapter.java
@@ -4,35 +4,106 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
 
 import com.daxh.explore.madtest02.R;
+import com.daxh.explore.madtest02.common.Header;
+import com.daxh.explore.madtest02.common.HeaderViewHolder;
 import com.daxh.explore.madtest02.common.Item;
 import com.daxh.explore.madtest02.common.ItemViewHolder;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Set;
 
-public class RecyclerViewWithHeadersAdapter extends RecyclerView.Adapter<ItemViewHolder> {
+public class RecyclerViewWithHeadersAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    private ArrayList<Item> items;
+    private static final int TYPE_HEADER    = 0x0001;
+    private static final int TYPE_ITEM      = 0x0002;
 
-    public RecyclerViewWithHeadersAdapter(ArrayList<Item> items) {
-        this.items = items;
+    private LinkedHashMap<Header, ArrayList<Item>> dataset;
+
+    private ArrayList<Object> items;
+    private ItemFilter itemFilter;
+
+    public RecyclerViewWithHeadersAdapter(LinkedHashMap<Header, ArrayList<Item>> dataset) {
+        this.dataset = dataset;
+
+        items = new ArrayList<>();
+        itemFilter = new ItemFilter();
+
+        itemFilter.filter("");
     }
 
     @Override
-    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_view, parent, false);
-        return new ItemViewHolder(itemView);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == TYPE_HEADER) {
+            View headerView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.header_view, parent, false);
+            return new HeaderViewHolder(headerView);
+        } else {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_view, parent, false);
+            return new ItemViewHolder(itemView);
+        }
     }
 
     @Override
-    public void onBindViewHolder(ItemViewHolder holder, int position) {
-        holder.bindItem(items.get(position));
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        int viewType = getItemViewType(position);
+        if (viewType == TYPE_HEADER) {
+            ((HeaderViewHolder)holder).bindItem((Header) items.get(position));
+        } else {
+            ((ItemViewHolder)holder).bindItem((Item) items.get(position));
+        }
     }
 
     @Override
     public int getItemCount() {
         return items == null ? 0 : items.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (items.get(position) instanceof Header) {
+            return TYPE_HEADER;
+        } else {
+            return TYPE_ITEM;
+        }
+    }
+
+    // According to official documentation:
+    // https://developer.android.com/reference/android/widget/Filter.html
+    // This class allows to execute filtering operations
+    // asynchronously and properly cancel that iterations
+    // that became un-relevant. Obviously, we using it here,
+    // but for flatting original dataset to items list that
+    // could be easily displayed. Probably there is a more
+    // true way to do it, but right now I didn't know it.
+    public class ItemFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence keyword) {
+            FilterResults results = new FilterResults();
+            final ArrayList<Object> tmpItemsList = new ArrayList<>();
+
+            final Set<Header> headers = dataset.keySet();
+            for (Header header : headers) {
+                tmpItemsList.add(header);
+                tmpItemsList.addAll(dataset.get(header));
+            }
+
+            results.values = tmpItemsList;
+            results.count = tmpItemsList.size();
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            items.clear();
+            items = (ArrayList<Object>) filterResults.values;
+            notifyDataSetChanged();
+        }
     }
 }
