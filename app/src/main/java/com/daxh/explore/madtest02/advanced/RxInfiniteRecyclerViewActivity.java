21213dd6ece6340ee5cc62f5b006a6a044a58d9d
Denis Suprun <suprundenis@gmail.com>
2017-01-17 19:20:42 +0300

    + RxInfiniteRecyclerViewActivity added, which for now is just a plain copy of InfiniteRecyclerViewActivity

---
+++ app/src/main/java/com/daxh/explore/madtest02/advanced/RxInfiniteRecyclerViewActivity.java
@@ -0,0 +1,142 @@
+package com.daxh.explore.madtest02.advanced;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import com.daxh.explore.madtest02.R;
+import com.daxh.explore.madtest02.common.Item;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class RxInfiniteRecyclerViewActivity extends AppCompatActivity {
+
+    private Button btLarge;
+    private Button btMedium;
+    private Button btSmall;
+
+    private RecyclerView rvItems;
+    private LinearLayoutManager llmItems;
+    private LinkedList<ArrayList<Object>> pages;
+    private AsyncTask<Object, Object, ArrayList<Object>> longRunningTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recyclerview);
+
+        llmItems = new LinearLayoutManager(this);
+        rvItems = (RecyclerView) findViewById(R.id.rvItems);
+        rvItems.setLayoutManager(llmItems);
+
+        btLarge = (Button) findViewById(R.id.btLarge);
+        btLarge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                parseData(R.array.lorem_ipsum);
+            }
+        });
+        btLarge.performClick();
+
+        btMedium = (Button) findViewById(R.id.btMedium);
+        btMedium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                parseData(R.array.medium);
+            }
+        });
+
+        btSmall = (Button) findViewById(R.id.btSmall);
+        btSmall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                parseData(R.array.small);
+            }
+        });
+    }
+
+    private void parseData(int resId){
+        if (longRunningTask != null && (!longRunningTask.isCancelled() || longRunningTask.getStatus() != AsyncTask.Status.FINISHED)) {
+            longRunningTask.cancel(true);
+        }
+
+        // Preparing data
+        String [] dataArray = getResources().getStringArray(resId);
+        pages = new LinkedList<>();
+        for (String data : dataArray) {
+            ArrayList<String> strings = new ArrayList<>(Arrays.asList(data.split(" ")));
+            ArrayList<Object> items = new ArrayList<>();
+            for (String s : strings) items.add(new Item(s));
+            pages.add(items);
+        }
+
+        // Creating adapter
+        RxInfiniteRecyclerViewAdapter adapter = new RxInfiniteRecyclerViewAdapter(pages.removeFirst(), (RxInfiniteRecyclerViewAdapter.Listener) new InfiniteScrollingListener());
+
+        // Setting up adapter for RecyclerView
+        rvItems.setAdapter(adapter);
+    }
+
+    private void asyncLoadNextPage(final RxInfiniteRecyclerViewAdapter adapter) {
+        // This is just a dirty example of long
+        // running longRunningTask. Obviously we shouldn't
+        // do such things in production code.
+        if (longRunningTask != null &&
+                longRunningTask.getStatus() != AsyncTask.Status.FINISHED) {
+            return;
+        }
+
+        longRunningTask = new AsyncTask<Object, Object, ArrayList<Object>>() {
+
+            @Override
+            protected void onPreExecute() {
+                adapter.showProgress(true);
+            }
+
+            @Override
+            protected ArrayList<Object> doInBackground(Object... voids) {
+                SystemClock.sleep(2000);
+                return pages.size() > 0 ? pages.removeFirst() : null;
+            }
+
+            @Override
+            protected void onPostExecute(ArrayList<Object> objects) {
+                if (objects != null && objects.size() > 0) {
+                    adapter.addAll(objects);
+                }
+                else {
+                    adapter.showProgress(false);
+                }
+            }
+        };
+        longRunningTask.execute();
+    }
+
+    public class InfiniteScrollingListener implements RxInfiniteRecyclerViewAdapter.Listener {
+        @Override
+        public void onNeedMoreData(RxInfiniteRecyclerViewAdapter adapter) {
+            asyncLoadNextPage(adapter);
+        }
+
+        @Override
+        public void onPlaceForMoreDataAvailable(RxInfiniteRecyclerViewAdapter adapter) {
+            asyncLoadNextPage(adapter);
+        }
+
+        @Override
+        public void onDataInserted(RxInfiniteRecyclerViewAdapter adapter) {
+            if (longRunningTask != null &&
+                    longRunningTask.getStatus() != AsyncTask.Status.FINISHED) {
+                return;
+            }
+            adapter.showProgress(false);
+        }
+    }
+}
---
+++ app/src/main/java/com/daxh/explore/madtest02/advanced/RxInfiniteRecyclerViewAdapter.java
@@ -0,0 +1,310 @@
+package com.daxh.explore.madtest02.advanced;
+
+import android.os.Handler;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+
+import com.daxh.explore.madtest02.R;
+import com.daxh.explore.madtest02.common.Item;
+import com.daxh.explore.madtest02.common.ItemViewHolder;
+import com.daxh.explore.madtest02.common.Progress;
+import com.daxh.explore.madtest02.common.ProgressViewHolder;
+
+import java.util.ArrayList;
+
+public class RxInfiniteRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final int TYPE_PROGRESS  = 0x0001;
+    private static final int TYPE_ITEM      = 0x0002;
+
+    private ArrayList<Object> originalItems;
+
+    private ArrayList<Object> attachedItems;
+    private ItemFilter itemFilter;
+
+    private boolean isProgressShown = false;
+    private Progress progress;
+
+    private Listener listener;
+    private OnScrollListener onScrollListener;
+    private DataObserver dataObserver;
+
+    private LinearLayoutManager llm;
+
+    private Handler handler = new Handler();
+
+    public RxInfiniteRecyclerViewAdapter(ArrayList<Object> items, Listener listener) {
+        this.originalItems = items;
+        this.listener = listener;
+
+        itemFilter = new ItemFilter();
+        onScrollListener = new OnScrollListener();
+        dataObserver = new DataObserver(this);
+    }
+
+    public Listener getListener() {
+        return listener;
+    }
+
+    public boolean isProgressShown() {
+        return isProgressShown;
+    }
+
+    public void showProgress(boolean show) {
+        if (show && !isProgressShown) {
+            isProgressShown = true;
+            progress = new Progress();
+            originalItems.add(progress);
+            unregisterAdapterDataObserver(dataObserver);
+                notifyItemInserted(originalItems.indexOf(progress));
+            registerAdapterDataObserver(dataObserver);
+        } else if(!show && isProgressShown) {
+
+            // This dirty trick allows us to show
+            // recyclerView animations in a right
+            // order: one after one, items inserted
+            // animation (when adding new items),
+            // item removed animation (when hiding
+            // progress item)
+
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    int pos = originalItems.indexOf(progress);
+                    originalItems.remove(progress);
+                    unregisterAdapterDataObserver(dataObserver);
+                        notifyItemRemoved(pos);
+                    registerAdapterDataObserver(dataObserver);
+                    progress = null;
+                    isProgressShown = false;
+                }
+            }, 0);
+        }
+    }
+
+    public void addAll(ArrayList<Object> items){
+        attachedItems = items;
+        itemFilter.filter("");
+    }
+
+    public int getOriginalItemCount() {
+        return originalItems == null ? 0 : isProgressShown ? originalItems.size()-1 : originalItems.size();
+    }
+
+    public boolean isPlaceForMoreDataAvailable() {
+        if (llm != null) {
+            int first = llm.findFirstCompletelyVisibleItemPosition();
+            int last = llm.findLastCompletelyVisibleItemPosition();
+
+            if (first == 0 && ((!isProgressShown && last == getItemCount()-1) ||
+                    (isProgressShown && last == getOriginalItemCount()))){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == TYPE_PROGRESS) {
+            View progressView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.progress_view, parent, false);
+            return new ProgressViewHolder(progressView);
+        } else {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_view, parent, false);
+            return new ItemViewHolder(itemView);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        int viewType = getItemViewType(position);
+        if (viewType == TYPE_PROGRESS) {
+            ((ProgressViewHolder)holder).bindItem((Progress) originalItems.get(position));
+        } else {
+            ((ItemViewHolder)holder).bindItem((Item) originalItems.get(position));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return originalItems == null ? 0 : originalItems.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (originalItems.get(position) instanceof Progress) {
+            return TYPE_PROGRESS;
+        } else {
+            return TYPE_ITEM;
+        }
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        recyclerView.addOnScrollListener(onScrollListener);
+        registerAdapterDataObserver(dataObserver);
+
+        if (llm == null) {
+            try{
+                llm = (LinearLayoutManager) recyclerView.getLayoutManager();
+            } catch (ClassCastException ignored) {
+            }
+        }
+    }
+
+    @Override
+    public void onDetachedFromRecyclerView(RecyclerView recyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView);
+        recyclerView.removeOnScrollListener(onScrollListener);
+        unregisterAdapterDataObserver(dataObserver);
+
+        llm = null;
+    }
+
+    // According to official documentation:
+    // https://developer.android.com/reference/android/widget/Filter.html
+    // This class allows to execute filtering operations
+    // asynchronously and properly cancel iterations that
+    // became un-relevant. Obviously, we using it here, but
+    // for appending new data to original dataset. Probably
+    // there is a more true way to do it, but right now I
+    // didn't know it.
+    public class ItemFilter extends Filter {
+
+        private int startPos;
+        private int insertionsNum;
+
+        @Override
+        protected FilterResults performFiltering(CharSequence keyword) {
+            FilterResults results = new FilterResults();
+            final ArrayList<Object> tmpItemsList = new ArrayList<>(originalItems);
+
+            startPos = originalItems.size()-(isProgressShown ? 1 : 0);
+            insertionsNum = attachedItems.size();
+            tmpItemsList.addAll(startPos, attachedItems);
+
+            attachedItems.clear();
+            attachedItems = null;
+
+            results.values = tmpItemsList;
+            results.count = tmpItemsList.size();
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            originalItems.clear();
+            originalItems = (ArrayList<Object>) filterResults.values;
+            notifyItemRangeInserted(startPos, insertionsNum);
+        }
+    }
+
+    public interface Listener {
+        void onNeedMoreData(RxInfiniteRecyclerViewAdapter adapter);
+
+        void onPlaceForMoreDataAvailable(RxInfiniteRecyclerViewAdapter adapter);
+
+        void onDataInserted(RxInfiniteRecyclerViewAdapter adapter);
+    }
+
+    public static class DataObserver extends RecyclerView.AdapterDataObserver {
+
+        private RxInfiniteRecyclerViewAdapter adapter;
+
+        public DataObserver(RxInfiniteRecyclerViewAdapter adapter) {
+            this.adapter = adapter;
+        }
+
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+            super.onItemRangeInserted(positionStart, itemCount);
+            if (adapter.getListener() != null) {
+
+                // This dirty trick allows us to show
+                // recyclerView animations in a right
+                // order
+
+                adapter.handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (adapter.isPlaceForMoreDataAvailable()) {
+                            adapter.getListener().onPlaceForMoreDataAvailable(adapter);
+                        }
+
+                        adapter.getListener().onDataInserted(adapter);
+                    }
+                }, 0);
+            }
+        }
+    }
+
+    public static class OnScrollListener extends RecyclerView.OnScrollListener {
+
+        private int offset = 3;
+        private LinearLayoutManager llm;
+        private RxInfiniteRecyclerViewAdapter adapter;
+
+        private boolean notify = true;
+
+        @Override
+        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+        }
+
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            if (adapter == null || llm == null) {
+                try{
+                    adapter = (RxInfiniteRecyclerViewAdapter) recyclerView.getAdapter();
+                    llm = (LinearLayoutManager) recyclerView.getLayoutManager();
+                } catch (ClassCastException e) {
+                    return;
+                }
+            }
+
+            // Catching the case when number of
+            // elements available for scrolling
+            // is less then offset
+            final int visibleItemsNum = llm.findLastVisibleItemPosition() - llm.findFirstVisibleItemPosition();
+            if (adapter.getItemCount() - visibleItemsNum < offset)
+                offset = adapter.getItemCount() - visibleItemsNum;
+
+            if (llm.findLastCompletelyVisibleItemPosition() >= adapter.getOriginalItemCount()-offset && notify){
+                notify = false;
+
+                /*
+                 *  We need this handler due to the following problem:
+                 *  Cannot call this method in a scroll callback. Scroll callbacks
+                 *  might be run during a measure & layout pass where you cannot
+                 *  change the RecyclerView data. Any method call that might change
+                 *  the structure of the RecyclerView or the adapter contents should
+                 *  be postponed to the next frame.
+                 *
+                 *  Solution described here:
+                 *
+                 *  http://stackoverflow.com/questions/39445330/cannot-call-notifyiteminserted-method-in-a-scroll-callback-recyclerview-v724-2
+                 */
+                recyclerView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (adapter.getListener() != null) {
+                            adapter.getListener().onNeedMoreData(adapter);
+                        }
+                    }
+                });
+            }
+
+            if (llm.findLastCompletelyVisibleItemPosition() < adapter.getOriginalItemCount()-offset && !notify){
+                notify = true;
+            }
+        }
+    }
+}
